a76642d41934806948421719be17b812b4e81c93
Justin1904 <justin1904@live.com>
2018-09-30 01:02:30 -0400

    complete pipeline from training to evaluation

--- assignment1/model.py
+++ assignment1/model.py
@@ -3,13 +3,26 @@
 """
 Basic seq2seq model with LSTMs and attention
 """
-
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.distributions as dist
 import torch.nn.functional as F
 from torch.nn.init import xavier_uniform_, orthogonal_
+from collections import namedtuple
+from utils import batch_iter
 
+
+Hypothesis = namedtuple('Hypothesis', ['value', 'score'])
+START_TOKEN_IDX = 1
+END_TOKEN_IDX = 2
+
+def pad(idx):
+    UNK_IDX = 0 # this is built-in into the vocab.py
+    max_len = max(map(len, idx))
+    for sent in idx:
+        sent += [0] * (max_len - len(sent))
+    return idx
 
 def dot_attn(a, b): # computes (batch_size, hidden_size) X (batch_size, max_seq_len, hidden_size) >> (batch_size, max_seq_len)
     return torch.einsum('bi,bji->bj', (a, b))
@@ -22,8 +35,8 @@ def lstm_cell_init_(lstm_cell):
     orthogonal_(lstm_cell.weight_hh.data)
     lstm_cell.bias_ih.data.fill_(0)
     lstm_cell.bias_hh.data.fill_(0)
-    lstm_cell.bias_ih.data[lstm_cell.hidden_size, 2*lstm_cell.hidden_size] = 1./2
-    lstm_cell.bias_hh.data[lstm_cell.hidden_size, 2*lstm_cell.hidden_size] = 1./2
+    lstm_cell.bias_ih.data[lstm_cell.hidden_size:2*lstm_cell.hidden_size] = 1./2
+    lstm_cell.bias_hh.data[lstm_cell.hidden_size:2*lstm_cell.hidden_size] = 1./2
 
 
 class LSTM(nn.Module):
@@ -96,31 +109,39 @@ class LSTMSeq2seq(nn.Module):
     An LSTM based seq2seq model with language as input.
 
     Args:
-         - vocab_size: the size of the vocabulary
+         - vocab: the vocab file from vocab.py
          - embedding_size: the size of the embedding
          - hidden_size: the size of the LSTM states, applies to encoder
          - bidirectional: whether or not the encoder is bidirectional
          - rdrop: whether there is a recurrent dropout on the encoder side
     '''
 
-    def __init__(self, vocab_size, embedding_size, hidden_size, bidirectional=True, rdrop=0.3):
+    def __init__(self, embedding_size, hidden_size, vocab, bidirectional=True, dropout_rate=0.3):
         super(LSTMSeq2seq, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_size)
-        self.encoder_lstm = LSTM(embedding_size, hidden_size, rdrop=rdrop, bidirectional=bidirectional)
+        self.vocab = vocab
+        self.src_vocab_size = len(vocab.src)
+        self.trg_vocab_size = len(vocab.tgt)
+        self.src_embedding = nn.Embedding(self.src_vocab_size, embedding_size)
+        self.trg_embedding = nn.Embedding(self.trg_vocab_size, embedding_size)
+        self.encoder_lstm = LSTM(embedding_size, hidden_size, rdrop=dropout_rate, bidirectional=bidirectional)
         self.decoder_lstm_cell = nn.LSTMCell(embedding_size, hidden_size * 2 if bidirectional else hidden_size)
-        self.decoder_output_layer = nn.Linear(hidden_size * 2 if bidirectional else hidden_size, vocab_size)
+        self.decoder_output_layer = nn.Linear(hidden_size * 4 if bidirectional else hidden_size * 2, self.trg_vocab_size)
 
-        self.vocab_size = vocab_size
         self.embedding_size = embedding_size
         self.hidden_size = hidden_size
         self.bidirectional = bidirectional
-        self.rdrop = rdrop
+        self.rdrop = dropout_rate
 
-    def apply_embedding(self, embedding_matrix):
-        '''
-        Turn an numpy matrix into torch tensor and plug it into the embedding layer
-        '''
-        self.embedding.weight = nn.Parameter(torch.from_numpy(embedding_matrix).float())
+    # def apply_embedding(self, embedding_matrix):
+    #     '''
+    #     Turn an numpy matrix into torch tensor and plug it into the embedding layer
+    #     '''
+    #     self.embedding.weight = nn.Parameter(torch.from_numpy(embedding_matrix).float())
+
+    def forward(self, src_tokens, src_lens, trg_tokens, trg_lens):
+        src_states, final_states = self.encode(src_tokens, src_lens)
+        ll = self.decode(src_states, final_states, src_lens, trg_tokens, trg_lens)
+        return ll
 
     def encode(self, src_tokens, src_lens):
         '''
@@ -130,11 +151,12 @@ class LSTMSeq2seq(nn.Module):
              - src_tokens: a torch tensor of a batch of tokens, with shape (batch_size, max_seq_len) >> LongTensor
              - src_lens: a torch tensor of the sentence lengths in the batch, with shape (batch_size,) >> LongTensor
         '''
-        src_vectors = self.embedding(src_tokens) # (batch_size, max_seq_len, embedding_size)
-        src_states, src_memory = self.encoder_lstm(src_vectors) # both (batch_size, max_seq_len, hidden_size (*2))
+        src_vectors = self.src_embedding(src_tokens) # (batch_size, max_seq_len, embedding_size)
+        src_states, _ = self.encoder_lstm(src_vectors) # both (batch_size, max_seq_len, hidden_size (*2))
 
         # need to use src_lens to pick out the actual last states of each sequence
-        final_states = src_states[:, src_lens, :] # (batch_size, hidden_size (*2))
+        batch_idx = torch.arange(0, src_states.size(0), out = src_states.new(0)).long()
+        final_states = src_states[batch_idx, src_lens-1, :] # (batch_size, hidden_size (*2))
         return src_states, final_states
     
     def decode(self, src_states, final_states, src_lens, trg_tokens, trg_lens, teacher_forcing=True, search_method='greedy'):
@@ -153,42 +175,128 @@ class LSTMSeq2seq(nn.Module):
         if search_method != 'greedy':
             raise NotImplementedError
         
-        trg_logits = []
+        nll = []
 
         # dealing with the start token
         h = final_states # (batch_size, hidden_size (*2))
         c = h.new_zeros(h.size(0), h.size(1), requires_grad=False)
         start_token = trg_tokens[..., 0] # (batch_size,)
-        vector = self.embedding(start_token) # (batch_size, embedding_size)
+        vector = self.trg_embedding(start_token) # (batch_size, embedding_size)
         h, c = self.decoder_lstm_cell(vector, (h, c))
         context_vector = LSTMSeq2seq.compute_attention(h, src_states, src_lens, attn_func=dot_attn) # (batch_size, hidden_size (*2))
         curr_logits = self.decoder_output_layer(torch.cat((h, context_vector), dim=-1)) # (batch_size, vocab_size)
+        neg_log_likelihoods = F.cross_entropy(curr_logits, trg_tokens[..., 1], reduction='none') # (batch_size,)
+        nll.append(neg_log_likelihoods)
         _, prd_token = torch.max(curr_logits, dim=-1) # (batch_size,) the decoded tokens
-        trg_logits.append(curr_logits)
         if teacher_forcing:
             prd_token = trg_tokens[..., 1] # feed the gold sequence token to the next time step
 
-        for t, token in enumerate(trg_tokens[1:]):
-            vector = self.embedding(token)
+        # input(trg_tokens.shape)
+        # TODO: check indexing
+        for t in range(trg_tokens.size(-1)-2):
+            token = trg_tokens[:, t+1]
+            vector = self.trg_embedding(token)
             h, c = self.decoder_lstm_cell(vector, (h, c))
             context_vector = LSTMSeq2seq.compute_attention(h, src_states, src_lens, attn_func=dot_attn)
-            curr_logits = self.decoder_output_layer(torch.cat(h, context_vector), dim=-1)
+            curr_logits = self.decoder_output_layer(torch.cat((h, context_vector), dim=-1))
+            neg_log_likelihoods = F.cross_entropy(curr_logits, trg_tokens[..., t+2], reduction='none') # (batch_size,)
+            nll.append(neg_log_likelihoods)
             _, prd_token = torch.max(curr_logits, dim=-1)
-            trg_logits.append(curr_logits)
             if teacher_forcing:
                 prd_token = trg_tokens[..., t+2]
         
         # computing the masked log-likelihood
-        trg_logits = torch.stack(trg_logits, dim=1) # (batch_size, max_seq_len, vocab_size)
-        log_likelihoods = F.cross_entropy(trg_logits, trg_tokens, reduction=None) # (batch_size, max_seq_len)
+        # trg_logits = torch.stack(trg_logits, dim=-1) # (batch_size, max_seq_len, vocab_size)
+        # neg_log_likelihoods = F.cross_entropy(trg_logits, trg_tokens[:, 1:], reduction='none') # (batch_size, max_seq_len-1) exclude <s> symbol
+        nll = torch.stack(nll, dim=1)
 
         # create masks, assuming padding is AT THE END
-        idx = torch.arange(0, src_states.size(1), out=curr_state.new(1).long()).unsqueeze(0)
-        mask = (idx < src_lens.unsqueeze(1)).float() # make use of the automatic expansion in comparison
-        masked_log_likelihoods = log_likelihoods * mask
+        idx = torch.arange(0, trg_tokens.size(1), out=trg_tokens.new(1).long()).unsqueeze(0)
+        mask = (idx < trg_lens.unsqueeze(1)).float() # make use of the automatic expansion in comparison
+        masked_log_likelihoods = - nll * mask[:, 1:] # exclude <s> token
 
-        return torch.mean(masked_log_likelihoods)
-    
+        return torch.sum(masked_log_likelihoods) # seems the training code assumes the log-likelihoods are summed per word
+
+    def beam_search(self, src_sent, src_lens, beam_size, max_decoding_step):
+        '''
+        Performs beam search decoding for testing the model. Currently just a fake method and only uses argmax decoding.
+        '''
+        self.training = False # turn of training
+        decoded_idx = []
+        scores = 0
+
+        src_states, final_state = self.encode(src_sent, src_lens)
+        h = final_state
+        c = h.new_zeros(h.size(0), h.size(1), requires_grad=False)
+        start_token = src_sent.new_ones((1,)).long() * START_TOKEN_IDX # (batch_size,) should be </s>
+        vector = self.trg_embedding(start_token) # (batch_size, embedding_size)
+        h, c = self.decoder_lstm_cell(vector, (h, c))
+        context_vector = LSTMSeq2seq.compute_attention(h, src_states, src_lens, attn_func=dot_attn) # (batch_size, hidden_size (*2))
+        curr_logits = self.decoder_output_layer(torch.cat((h, context_vector), dim=-1)) # (batch_size, vocab_size)
+        curr_ll = F.log_softmax(curr_logits, dim=-1) # transform logits into log-likelihoods
+        curr_score, prd_token = torch.max(curr_ll, dim=-1) # (batch_size,) the decoded tokens
+        decoded_idx.append(prd_token.item())
+        scores += curr_score.item()
+
+        decoding_step = 1
+        while decoding_step <= max_decoding_step and prd_token.item() != END_TOKEN_IDX:
+            vector = self.trg_embedding(prd_token)
+            h, c = self.decoder_lstm_cell(vector, (h, c))
+            context_vector = LSTMSeq2seq.compute_attention(h, src_states, src_lens, attn_func=dot_attn)
+            curr_logits = self.decoder_output_layer(torch.cat(h, context_vector), dim=-1)
+            curr_ll = F.log_softmax(curr_logits, dim=-1) # transform logits into log-likelihoods
+            curr_score, prd_token = torch.max(curr_ll, dim=-1)
+            decoded_idx.append(prd_token.item())
+            scores += curr_score.item()
+
+        sentence = list(map(lambda x: self.vocab.tgt.id2word[x], decoded_idx))
+        greedy_hyp = Hypothesis(sentence, scores)
+        self.training = True # turn training back on
+        return [greedy_hyp] * beam_size
+
+    def evaluate_ppl(self, dev_data, batch_size, cuda=True):
+        """
+        Evaluate perplexity on dev sentences
+
+        Args:
+            dev_data: a list of dev sentences
+            batch_size: batch size
+        
+        Returns:
+            ppl: the perplexity on dev sentences
+        """
+        self.training = False
+        cum_loss = 0.
+        cum_tgt_words = 0.
+
+        # you may want to wrap the following code using a context manager provided
+        # by the NN library to signal the backend to not to keep gradient information
+        # e.g., `torch.no_grad()`
+
+        if cuda:
+            torch.LongTensor = torch.cuda.LongTensor
+
+        for src_sents, tgt_sents in batch_iter(dev_data, batch_size):
+            src_lens = torch.LongTensor(list(map(len, src_sents)))
+            trg_lens = torch.LongTensor(list(map(len, tgt_sents)))
+            
+            # these padding functions modify data in-place
+            src_sents = pad(self.vocab.src.words2indices(src_sents))
+            tgt_sents = pad(self.vocab.tgt.words2indices(tgt_sents))
+
+            src_sents = torch.LongTensor(src_sents)
+            tgt_sents = torch.LongTensor(tgt_sents)
+            loss = -self.forward(src_sents, src_lens, tgt_sents, trg_lens).sum()
+
+            loss = loss.item()
+            cum_loss += loss
+            tgt_word_num_to_predict = sum(len(s[1:]) for s in tgt_sents)  # omitting the leading `<s>`
+            cum_tgt_words += tgt_word_num_to_predict
+
+        ppl = np.exp(cum_loss / cum_tgt_words)
+        self.training = True
+        return ppl
+
     @staticmethod
     def compute_attention(curr_state, src_states, src_lens, attn_func):
         '''
@@ -215,3 +323,21 @@ class LSTMSeq2seq(nn.Module):
 
         context_vector = torch.einsum('bij,bi->bj', (src_states, attn_weights))
         return context_vector
+
+    @staticmethod
+    def load(model_path):
+        """
+        Load a pre-trained model
+
+        Returns:
+            model: the loaded model
+        """
+        model = torch.load(model_path)
+        return model
+
+    def save(self, path):
+        """
+        Save current model to file
+        """
+        torch.save(model, path)
+        return
