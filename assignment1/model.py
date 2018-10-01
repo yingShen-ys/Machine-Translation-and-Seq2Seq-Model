Fix issues with perplexity computation

assignment1/model.py
@@ -506,6 +506,8 @@ class OLSTMSeq2seq(nn.Module):
             torch.LongTensor = torch.cuda.LongTensor
 
         for src_sents, tgt_sents in batch_iter(dev_data, batch_size):
+            tgt_word_num_to_predict = sum(len(s[1:]) for s in tgt_sents)  # omitting the leading `<s>`
+            cum_tgt_words += tgt_word_num_to_predict
             src_lens = torch.LongTensor(list(map(len, src_sents)))
             trg_lens = torch.LongTensor(list(map(len, tgt_sents)))
             
@@ -519,8 +521,6 @@ class OLSTMSeq2seq(nn.Module):
 
             loss = loss.item()
             cum_loss += loss
-            tgt_word_num_to_predict = sum(len(s[1:]) for s in tgt_sents)  # omitting the leading `<s>`
-            cum_tgt_words += tgt_word_num_to_predict
 
         ppl = np.exp(cum_loss / cum_tgt_words)
         self.training = True