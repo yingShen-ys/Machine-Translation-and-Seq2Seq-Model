3abcedc4bd32c3e83ed540a36dd967938c6bea7e
YingShen <yingshen.cs@gmail.com>
2018-10-28 15:09:10 -0400

    ignore trained word_embed

--- assignment-2/scripts/train.sh
+++ assignment-2/scripts/train.sh
@@ -31,34 +31,34 @@ work_dir="work_dir_basic_concatenate_${source}_${auxiliary}"
 mkdir -p ${work_dir}
 echo save results to ${work_dir}
 
-python nmt_mk_iii.py \
-    train \
-    --model-type original_lstm \
-    --cuda \
-    --seed 233 \
-    --vocab ${vocab} \
-    --train-src ${train_src} \
-    --train-tgt ${train_tgt} \
-    --dev-src ${dev_src} \
-    --dev-tgt ${dev_tgt} \
-    --save-to ${work_dir}/model.bin \
-    --valid-niter 2400 \
-    --batch-size 64 \
-    --hidden-size 256 \
-    --embed-size 256 \
-    --uniform-init 0.1 \
-    --dropout 0.2 \
-    --clip-grad 5.0 \
-    --lr-decay 0.5 2>${work_dir}/err.log
-
-python nmt_mk_iii.py \
-    decode \
-    --seed 233 \
-    --beam-size 5 \
-    --max-decoding-time-step 100 \
-    ${vocab} \
-    ${work_dir}/model.bin \
-    ${test_src} \
-    ${work_dir}/decode.txt
-
-perl multi-bleu.perl ${test_tgt} < ${work_dir}/decode.txt
+#python nmt_mk_iii.py \
+#    train \
+#    --model-type original_lstm \
+#    --cuda \
+#    --seed 233 \
+#    --vocab ${vocab} \
+#    --train-src ${train_src} \
+#    --train-tgt ${train_tgt} \
+#    --dev-src ${dev_src} \
+#    --dev-tgt ${dev_tgt} \
+#    --save-to ${work_dir}/model.bin \
+#    --valid-niter 2400 \
+#    --batch-size 64 \
+#    --hidden-size 256 \
+#    --embed-size 256 \
+#    --uniform-init 0.1 \
+#    --dropout 0.2 \
+#    --clip-grad 5.0 \
+#    --lr-decay 0.5 2>${work_dir}/err.log
+#
+#python nmt_mk_iii.py \
+#    decode \
+#    --seed 233 \
+#    --beam-size 5 \
+#    --max-decoding-time-step 100 \
+#    ${vocab} \
+#    ${work_dir}/model.bin \
+#    ${test_src} \
+#    ${work_dir}/decode.txt
+#
+#perl multi-bleu.perl ${test_tgt} < ${work_dir}/decode.txt
