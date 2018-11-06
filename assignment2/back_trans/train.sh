ee2efeb74503026035e3c2cde07313adc7067aeb
YingShen <yingshen.cs@gmail.com>
2018-11-06 15:57:33 -0500

    tunning w/o aux

--- assignment2/back_trans/train.sh
+++ assignment2/back_trans/train.sh
@@ -1,7 +1,8 @@
-lan="tr"
-~/mosesdecoder/scripts/training/train-model.perl -root-dir train-${lan} \
+src=$1
+aux=$2
+~/mosesdecoder/scripts/training/train-model.perl -root-dir train-${src}${aux}-en \
 -cores 4 \
--corpus ~/data/processed_para/train.en-${lan}.clean \
--f en -e ${lan} -alignment grow-diag-final-and -reordering msd-bidirectional-fe \
--lm 0:3:$HOME/lm/wiki.${lan}.bin:8                          \
+-corpus ~/data/processed_para/train.en-${src}${aux}.clean \
+-f ${src}${aux} -e en -alignment grow-diag-final-and -reordering msd-bidirectional-fe \
+-lm 0:3:$HOME/lm/ted.en.bin:8                          \
 -external-bin-dir ~/mosesdecoder/tools
--- assignment2/back_trans/tunning_src.sh
+++ assignment2/back_trans/tunning_src.sh
@@ -1,8 +1,8 @@
 src=$1
 aux=$2
 $HOME/mosesdecoder/scripts/training/mert-moses.pl \
-$HOME/data/processed_para/dev.en-${src}${aux}.clean.${src}${aux} $HOME/data/processed_para/dev.en-${src}${aux}.clean.en \
+$HOME/data/processed_para/dev.en-${src}.clean.${src} $HOME/data/processed_para/dev.en-${src}.clean.en \
 $HOME/mosesdecoder/bin/moses $HOME/working/train-${src}${aux}-en/model/moses.ini --mertdir $HOME/mosesdecoder/bin/ \
 --rootdir $HOME/mosesdecoder/scripts \
---working-dir=$HOME/working/mert-work-${src}${aux}-en \
+--working-dir=$HOME/working/mert-work-${src}-tune-en \
 --decoder-flags="-threads 4"
